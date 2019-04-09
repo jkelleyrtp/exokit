afabbb56a2ad5b3598b6e84e845a1f3fb522411f
Avaer Kazmer <a@modules.io>
2019-04-09 04:37:54 -0400

    Major oculus mobile VR entry debugging

--- src/index.js
+++ src/index.js
@@ -776,7 +776,7 @@ if (nativeBindings.nativeOculusMobileVr) {
 
         // fps = VR_FPS;
 
-        const vrContext = oculusMobileVrPresentState.vrContext = oculusMobileVrPresentState.vrContext || nativeBindings.nativeOculusMobileVr.OculusMobile_Init();
+        const vrContext = oculusMobileVrPresentState.vrContext = oculusMobileVrPresentState.vrContext || nativeBindings.nativeOculusMobileVr.OculusMobile_Init(context.getWindowHandle());
 
         const {width: halfWidth, height} = vrContext.GetRecommendedRenderTargetSize();
         const MAX_TEXTURE_SIZE = 4096;
@@ -1714,9 +1714,7 @@ const _startRenderLoop = () => {
         timestamps.last = now;
       }
     } else if (oculusMobileVrPresentState.vrContext) {
-      const windowHandle = oculusMobileVrPresentState.glContext.getWindowHandle();
       oculusMobileVrPresentState.hasPose = oculusMobileVrPresentState.vrContext.WaitGetPoses(
-        windowHandle,
         oculusMobilePoseFloat32Array
       );
 
